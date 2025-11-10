48937bd32efd53b388e9590b14910ea6e77ca9b4
Keishmer Pérez <145237779+Keishmer00@users.noreply.github.com>
2025-11-09 19:57:36 -0500

    act

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-const isProd = process.env.NODE_ENV === 'production';
+
 
 export default defineConfig({
   site: 'https://riografa.github.io',
-  base: isProd ? '/riografa.github.io/' : '/',
+  base: '/',
   integrations: [tailwind()],
   output: 'static',
   build: { assets: 'assets' },
