3a2c6b934a853e71e556bdf5e5f459fbc57bed0b
Keishmer Pérez <145237779+Keishmer00@users.noreply.github.com>
2025-11-09 21:05:48 -0500

    act

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-
-
 export default defineConfig({
   site: 'https://riografa.github.io',
-  base: '/luisagiraldoportfolio',
+  base: process.env.NODE_ENV === 'production' ? '/luisagiraldoportfolio/' : '/', 
   integrations: [tailwind()],
   output: 'static',
-  build: { assets: 'assets' },
 });
