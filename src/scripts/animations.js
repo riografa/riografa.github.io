23c67cd809ae5ec214e8afdc8fac688fddcc9271
Keishmer Pérez <145237779+Keishmer00@users.noreply.github.com>
2026-03-02 10:03:05 -0500

    act

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -3,10 +3,10 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ScrollSmoother } from "gsap/ScrollSmoother";
 
-gsap.registerPlugin(ScrollTrigger,ScrollSmoother);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-gsap.from('#navbar'), {
+gsap.from('#navbar', {
     y: -100,
     duration: 1,
-    ease: "bounce.out" , 
-};
+    ease: "bounce.out"
+});
